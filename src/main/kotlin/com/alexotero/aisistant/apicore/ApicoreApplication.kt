7e70595e958a44758ca4f7036f95486d6ab8e57d
Alex <alexoterof@gmail.com>
2023-03-18 19:56:41 +0100

    First commit

---
+++ src/main/kotlin/com/alexotero/aisistant/apicore/ApicoreApplication.kt
@@ -0,0 +1,11 @@
+package com.alexotero.aisistant.apicore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApicoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApicoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/alexotero/aisistant/apicore/ApicoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alexotero.aisistant.apicore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApicoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
